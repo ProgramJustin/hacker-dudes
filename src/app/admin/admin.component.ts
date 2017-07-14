a06c493dfe523b7b9f87dee1daeff664402db5f9
Justin Gomez <programjustin@gmail.com>
2017-07-14 14:44:15 -0700

    add submit form fucntion

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { HackerService } from '../hacker.service';
+import { Hacker } from '../hacker.model';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css']
+  styleUrls: ['./admin.component.css'],
+  providers: [HackerService]
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  constructor(private hackerService: HackerService) { }
 
   ngOnInit() {
+  }
+  submitForm(alias: string, skill: string, description: string) {
+    var newHacker: Hacker = new Hacker(alias, skill, description);
+    this.hackerService.addHacker(newHacker);
   }
 
 }
