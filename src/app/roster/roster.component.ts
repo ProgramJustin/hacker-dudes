609ef63c79dca08a1c1fc930e4c90d17325dd316
Justin Gomez <programjustin@gmail.com>
2017-07-14 15:29:09 -0700

    add property containing current route

--- src/app/roster/roster.component.ts
+++ src/app/roster/roster.component.ts
@@ -12,14 +12,16 @@ import { FirebaseListObservable } from 'angularfire2/database';
   providers: [HackerService]
 })
 export class RosterComponent implements OnInit {
-
   hackers: FirebaseListObservable<any[]>;
+  currentRoute: string = this.router.url;
 
   constructor(private router: Router, private hackerService: HackerService) { }
 
   ngOnInit(){
-  this.hackers = this.hackerService.getHackers();
-}
+    this.hackers = this.hackerService.getHackers();
+    console.log(this.router.url);
+  }
+
 
 goToDetailPage(clickedHacker) {
   this.router.navigate(['hackers', clickedHacker.$key]);
