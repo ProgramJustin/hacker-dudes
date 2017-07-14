06c419c955d7f540f3aa3be2611fd614cca3d105
Justin Gomez <programjustin@gmail.com>
2017-07-14 11:43:25 -0700

    import modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { routing } from './app.routing';
 
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
+import { HackerComponent } from './hacker/hacker.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent
+    AboutComponent,
+    HackerComponent
   ],
   imports: [
     BrowserModule
